8aa183c27f090a6da83b56415051619e30d476a4
Bob Skowron <bob.skowron@gmail.com>
2021-12-02 21:24:53 -0600

    Reorganize to support unit testing setup

--- day-2/day_2.jl
+++ day-2/day_2.jl
@@ -1,33 +1,49 @@
+module Day2
 
-f = open("day-2/puzzle-input.txt")
-lines = readlines(f)
+    export parse_file, calc_position_depth, calc_position_aim_depth
 
-dir_array = [(split(x, " ")[1], parse(Int64, split(x, " ")[2]))  for x in lines]
+    function parse_file(file_path)
+        f = open(file_path)
+        lines = readlines(f)    
 
-# part 1
-horizontal_position = sum([x[2] * (x[1] == "forward") for x in dir_array])
-depth = sum([x[2] * (x[1] == "up" ? -1 : (x[1] == "down" ? 1 : 0)) for x in dir_array])
+        dir_array = [(split(x, " ")[1], parse(Int64, split(x, " ")[2]))  for x in lines]
 
-print("Depth x Horizonal Position: ", horizontal_position*depth)
-
-# part 2
-function calc_position_aim_depth(dir_array)
-    (horizontal_position, aim, depth) = (0,0,0)
-    for (dir, val) in dir_array
-        if dir == "forward"
-            horizontal_position += val
-            depth += (aim * val)
-        elseif dir == "down"
-            aim += val
-        elseif dir == "up"
-            aim -= val
-        else
-            println("whoops")
-        end
+        return dir_array
     end
 
-    return (horizontal_position, aim, depth)
+    # part 1
+    function calc_position_depth(dir_array)
+        horizontal_position = sum([x[2] * (x[1] == "forward") for x in dir_array])
+        depth = sum([x[2] * (x[1] == "up" ? -1 : (x[1] == "down" ? 1 : 0)) for x in dir_array])
+
+        return (horizontal_position, depth)
+    end
+
+    # part 2
+    function calc_position_aim_depth(dir_array)
+        (horizontal_position, aim, depth) = (0,0,0)
+        for (dir, val) in dir_array
+            if dir == "forward"
+                horizontal_position += val
+                depth += (aim * val)
+            elseif dir == "down"
+                aim += val
+            elseif dir == "up"
+                aim -= val
+            else
+                println("whoops")
+            end
+        end
+
+        return (horizontal_position, aim, depth)
+    end
 end
 
+
+dir_array = parse_file("day-2/puzzle-input.txt")
+
+(horizontal_position, depth) = calc_position_depth(dir_array)
+print("Part1 - Depth x Horizonal Position: ", horizontal_position*depth)
+
 (horizontal_position, aim, depth) = calc_position_aim_depth(dir_array)
-print("Depth x Horizonal Position: ", horizontal_position*depth)
+print("Part 2 - Depth x Horizonal Position: ", horizontal_position*depth)
