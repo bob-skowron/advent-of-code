df1bf893e456b5c984a4b95522b822185e4f1315
Bob Skowron <bob.skowron@gmail.com>
2021-12-04 23:07:35 -0600

    Add details for day 5

---
+++ day-5/tests/test_day_5.jl
@@ -0,0 +1,36 @@
+include("../day_5.jl")
+using Test
+using .Day5
+
+# (called_numbers, boards) = Day4.parse_file("day-4/tests/test-input.txt")
+
+# @testset "Day 4" begin
+#     @test called_numbers == [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
+#     @test boards == [
+#         [
+#             22 13 17 11  0;
+#             8  2 23  4 24;
+#             21  9 14 16  7;
+#             6 10  3 18  5;
+#             1 12 20 15 19
+#         ],
+#         [
+#             3 15  0  2 22;
+#             9 18 13 17  5;
+#            19  8  7 25 23;
+#            20 11 10 24  4;
+#            14 21 16 12  6
+#         ],
+#         [
+#             14 21 17 24  4;
+#             10 16 15  9 19;
+#             18  8 23 26 20;
+#             22 11 13  6  5;
+#             2  0 12  3  7;
+#         ]
+#     ]
+
+#      @test Day4.calc_boards(called_numbers, boards) == (24, 188)
+
+#      @test Day4.calc_last_board(called_numbers, boards) == (13, 148)
+# end
