c7caa9e875e2c5f75212ec48b569f1e7fdaa139b
Bob Skowron <bob.skowron@gmail.com>
2021-12-05 21:05:34 -0600

    Part 2 completed

--- day-5/day_5.jl
+++ day-5/day_5.jl
@@ -1,6 +1,6 @@
 module Day5
 
-    export parse_file, calc_number_crossings
+    export parse_file, calc_number_crossings, calc_number_crossings_with_diags
 
     function parse_file(file_path)
         f = open(file_path)
@@ -33,10 +33,56 @@ module Day5
         return sum(diagram .>= 2)
     end
 
-   
+   # part 2
+   function calc_number_crossings_with_diags(line_coords)
+        diagram = zeros(maximum(line_coords)+1,maximum(line_coords)+1)
+
+        for i=1:size(line_coords)[1]
+            if (line_coords[i,1] .== line_coords[i,3]) | (line_coords[i,2] .== line_coords[i,4]) # handle straight lines here
+                min_x = min(line_coords[i,2], line_coords[i,4]) + 1
+                max_x = max(line_coords[i,2], line_coords[i,4]) + 1
+        
+                min_y = min(line_coords[i,1], line_coords[i,3]) + 1
+                max_y = max(line_coords[i,1], line_coords[i,3]) + 1
+
+                diagram[
+                    min_x:max_x, # reversed here
+                    min_y:max_y] .+= 1
+            else # diagonals
+                complete = Bool(0)
+                current_x = line_coords[i,2] + 1
+                current_y = line_coords[i,1] + 1
+
+                while(!complete)
+                    diagram[current_x, current_y] += 1
+
+                    if (current_x == line_coords[i,4] + 1) & (current_y == line_coords[i,3] + 1)
+                        complete = Bool(1)
+                    else
+                        if line_coords[i,2] <= line_coords[i,4]
+                            current_x += 1
+                        else
+                            current_x -= 1
+                        end
+
+                        if line_coords[i,1] <= line_coords[i,3]
+                            current_y += 1
+                        else
+                            current_y -= 1
+                        end
+                    end               
+                end
+            end
+        end
+
+        return sum(diagram .>= 2)
+    end
+
 end
 
 line_coords = parse_file("day-5/puzzle-input.txt")
 crossings = calc_number_crossings(line_coords)
 println("Part 1 - number crossings: ", crossings)
 
+crossings_diags = calc_number_crossings_with_diags(line_coords)
+println("Part 2 - number crossings with diags: ", crossings_diags)
--- day-5/tests/test_day_5.jl
+++ day-5/tests/test_day_5.jl
@@ -4,21 +4,21 @@ using .Day5
 
 line_coords = Day5.parse_file("day-5/tests/test-input.txt")
 
-@testset "Day 4" begin
+@testset "Day 5" begin
     @test line_coords ==  [
-        0, 9, 5, 9;
-        8, 0, 0, 8;
-        9, 4, 3, 4;
-        2, 2, 2, 1;
-        7, 0, 7, 4;
-        6, 4, 2, 0;
-        0, 9, 2, 9;
-        3, 4, 1, 4;
-        0, 0, 8, 8;
-        5, 5, 8, 2;
+        0 9 5 9;
+        8 0 0 8;
+        9 4 3 4;
+        2 2 2 1;
+        7 0 7 4;
+        6 4 2 0;
+        0 9 2 9;
+        3 4 1 4;
+        0 0 8 8;
+        5 5 8 2;
     ]
 
     @test Day5.calc_number_crossings(line_coords) == 5
 
-    #  @test Day4.calc_last_board(called_numbers, boards) == (13, 148)
+    @test Day5.calc_number_crossings_with_diags(line_coords) == 12
 end
