a0d3f04a8f7d65ae73e439c94520f162cf7c6609
Bob Skowron <bob.skowron@gmail.com>
2021-12-05 20:39:56 -0600

    Successfully completed part 1

--- day-5/day_5.jl
+++ day-5/day_5.jl
@@ -1,4 +1,42 @@
 module Day5
 
+    export parse_file, calc_number_crossings
+
+    function parse_file(file_path)
+        f = open(file_path)
+        full_file = readlines(f)
+
+        line_coords = [parse.(Int, match(r"(\d*),(\d*) -> (\d*),(\d*)", x).captures) for x in full_file]
+
+        return transpose(hcat(line_coords...))
+    end
+
+    function calc_number_crossings(line_coords)
+        # filter out any non-vertical/horizonal
+        filtered_coords = line_coords[(line_coords[:,1] .== line_coords[:,3]) .| (line_coords[:,2] .== line_coords[:,4]),:]
+
+        diagram = zeros(maximum(filtered_coords)+1,maximum(filtered_coords)+1)
+
+        for i=1:size(filtered_coords)[1]
+            min_x = min(filtered_coords[i,2], filtered_coords[i,4]) + 1
+            max_x = max(filtered_coords[i,2], filtered_coords[i,4]) + 1
+
+            min_y = min(filtered_coords[i,1], filtered_coords[i,3]) + 1
+            max_y = max(filtered_coords[i,1], filtered_coords[i,3]) + 1
+
+            diagram[
+                min_x:max_x, # reversed here
+                min_y:max_y] .+= 1
+
+        end
+
+        return sum(diagram .>= 2)
+    end
+
    
 end
+
+line_coords = parse_file("day-5/puzzle-input.txt")
+crossings = calc_number_crossings(line_coords)
+println("Part 1 - number crossings: ", crossings)
+
--- day-5/tests/test_day_5.jl
+++ day-5/tests/test_day_5.jl
@@ -2,35 +2,23 @@ include("../day_5.jl")
 using Test
 using .Day5
 
-# (called_numbers, boards) = Day4.parse_file("day-4/tests/test-input.txt")
+line_coords = Day5.parse_file("day-5/tests/test-input.txt")
 
-# @testset "Day 4" begin
-#     @test called_numbers == [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
-#     @test boards == [
-#         [
-#             22 13 17 11  0;
-#             8  2 23  4 24;
-#             21  9 14 16  7;
-#             6 10  3 18  5;
-#             1 12 20 15 19
-#         ],
-#         [
-#             3 15  0  2 22;
-#             9 18 13 17  5;
-#            19  8  7 25 23;
-#            20 11 10 24  4;
-#            14 21 16 12  6
-#         ],
-#         [
-#             14 21 17 24  4;
-#             10 16 15  9 19;
-#             18  8 23 26 20;
-#             22 11 13  6  5;
-#             2  0 12  3  7;
-#         ]
-#     ]
+@testset "Day 4" begin
+    @test line_coords ==  [
+        0, 9, 5, 9;
+        8, 0, 0, 8;
+        9, 4, 3, 4;
+        2, 2, 2, 1;
+        7, 0, 7, 4;
+        6, 4, 2, 0;
+        0, 9, 2, 9;
+        3, 4, 1, 4;
+        0, 0, 8, 8;
+        5, 5, 8, 2;
+    ]
 
-#      @test Day4.calc_boards(called_numbers, boards) == (24, 188)
+    @test Day5.calc_number_crossings(line_coords) == 5
 
-#      @test Day4.calc_last_board(called_numbers, boards) == (13, 148)
-# end
+    #  @test Day4.calc_last_board(called_numbers, boards) == (13, 148)
+end
